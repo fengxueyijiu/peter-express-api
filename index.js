13bbd1de44f173b4a5868bed09c03276bbb8d88d
Your Name <you@example.com>
2016-09-27 15:45:07 +0800

    2

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  console.log("aaa")
+  var page='<html>'+
+            "<body>"+
+            "<h1>Index.html</h1>"+
+            "</body>"+
+            "</html>"
+
+  res.send(page)
 })
-app.get('/peter', function (req, res) {
-    console.log("bbbb")
+app.get('/about.html', function (req, res) {
+  var page='<html>'+
+            "<body>"+
+            "<h1> About.html</h1>"+
+            "</body>"+
+            "</html>"
+
+  res.send(page)
 })
 app.listen(3000,function(){
     console.log("running on port 3000...")
